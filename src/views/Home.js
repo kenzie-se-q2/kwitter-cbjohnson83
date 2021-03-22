bb09932ac215c44d6bf4e131aa108ff70d82d1bf
PUT YOUR NAME HERE <YOUREMAIL@YOUREMAIL.com>
2021-03-22 13:52:17 -0400

    changes color navbar align

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,16 +2,23 @@ import React from "react";
 import Login from "../components/Login";
 import Menu from "../components/Menu";
 import { useStore } from "../store/store";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Home(props) {
   const user = useStore((state) => state.user);
   return (
     <>
       <Menu />
-      <h2>Your favorite microblogging platform</h2>
+      <div id="shareIdeas">
+        <h2>A Place to Share Ideas. </h2>
+      </div>
+      <div id="noHate">
+        <h3>NO HATE SPEACH ALLOWED!</h3>
+      </div>
       {!user.token && <Login />}
-     <p>New User? <Link to = "/NewAccount">Create Account</Link></p>
+      <p>
+        New User? <Link to="/NewAccount">Create Account</Link>
+      </p>
     </>
   );
 }
--- src/views/Navbar.js
+++ src/views/Navbar.js
@@ -4,12 +4,18 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <div id>
+      <div id="nav">
         {/* <NavLink></NavLink> */}
         {/* <NavMenu> */}
-        <Link to="/about">About</Link>
-        <Link to="/sign-up">Sign Up</Link>
-        <Link to="/messages">Contact Us</Link>
+        <div id="navAbout">
+          <Link to="/about">About</Link>
+        </div>
+        <div id="navSignUp">
+          <Link to="/sign-up">Sign Up</Link>
+        </div>
+        <div id="contactUs">
+          <Link to="/contactUs">Contact Us</Link>
+        </div>
         {/* </NavMenu> */}
         {/* <NavBtn></NavBtn> */}
       </div>
