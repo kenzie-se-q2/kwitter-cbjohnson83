3fd0916b70ee929e5a6639edfd0f052155b0b428
Cassandra Johnson <cassandra.johnson83@outlook.com>
2021-03-26 15:40:01 -0600

    Added logout to user delete

--- src/components/DeleteUser.js
+++ src/components/DeleteUser.js
@@ -1,35 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { Link, Redirect } from "react-router-dom";
-
-import { DeleteuserRequest } from "../fetchRequests";
+import { Redirect } from "react-router-dom";
+import { DeleteuserRequest, logoutRequest } from "../fetchRequests";
 import { useStore } from "../store/store";
 
 const DeleteUser = () => {
   const currentUser = useStore((state) => state.user);
   //const userToken = useStore(state =>state.token)
   const [deleteUser, setDeleteUser] = useState(false);
+  const dispatch = useStore((state) => state.dispatch);
+
   const handleOnClick = () => {
     setDeleteUser(true);
     DeleteuserRequest(currentUser.token, currentUser.username).then((res) =>
       console.log(res)
     );
+    logoutRequest(currentUser.token).then(() => dispatch({ type: "LOGOUT" }));
   };
+
   useEffect(() => {
     if (deleteUser === true) {
       alert("User Deletion Successful");
-      //   window.location.reload(true);
     }
     return () => {
       if (deleteUser === false) {
-        // window.location.reload(false);
       }
     };
   }, [deleteUser]);
   return (
     <div>
-      <Link to="/">
-        <button onClick={handleOnClick}>Delete Profile</button>
-      </Link>
+      <button onClick={handleOnClick}>Delete Profile</button>
       {deleteUser && <Redirect to="/" />}
     </div>
   );
